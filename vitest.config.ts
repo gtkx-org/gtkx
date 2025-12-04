3b3f05c9ad8b372afbc65c089730fdef34850a75
Eugenio Depalo <eugeniodepalo@gmail.com>
2025-12-04 23:22:12 +0000

    v0.1.32

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    resolve: {
-        conditions: ["development", "default"],
-    },
     test: {
         include: ["tests/**/*.test.{ts,tsx}"],
         typecheck: {
