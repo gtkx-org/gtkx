d67eab40d94b85a7fe34935e677430fb152e9a13
Eugenio Depalo <eugeniodepalo@gmail.com>
2026-05-02 00:08:35 +0100

    Add --ignore-scripts to npx postject

--- examples/tutorial/scripts/build-sea.sh
+++ examples/tutorial/scripts/build-sea.sh
@@ -26,7 +26,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 echo "Injecting SEA blob..."
-npx postject dist/app NODE_SEA_BLOB dist/sea-prep.blob \
+npx --ignore-scripts postject dist/app NODE_SEA_BLOB dist/sea-prep.blob \
     --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
 
 chmod +x dist/app
