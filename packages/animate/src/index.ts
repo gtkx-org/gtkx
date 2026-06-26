13c4b001396bf91299d439fe6d8e6e4e3dc97471
Eugenio Depalo <eugeniodepalo@gmail.com>
2026-06-26 14:40:33 +0200

    Add animated factory and widget-only proxy

--- packages/animate/src/index.ts
+++ packages/animate/src/index.ts
@@ -1,4 +1,5 @@
 export { AnimatePresence } from "./animate-presence.js";
+export { type AnimatedComponent, type AnimationProps, animated } from "./animated.js";
 export type { AnimationTarget, Easing, RepeatType, Transition, WidgetAnimationProps } from "./types.js";
 export { useWidgetAnimation } from "./use-widget-animation.js";
 export { WidgetAnimation } from "./widget-animation.js";
