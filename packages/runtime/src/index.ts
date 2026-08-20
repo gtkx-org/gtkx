a845415689e00d3b727ba6a309916a24ea817a6b
Eugenio Depalo <eugeniodepalo@gmail.com>
2026-08-20 20:16:09 +0200

    Move regex subject retention into the runtime

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -26,6 +26,8 @@ export {
 /** @public */
 export { promisify } from "./promisify.js";
 /** @public */
+export { matchAllRegex, matchRegex } from "./regex.js";
+/** @public */
 export { type Interface, registerClass } from "./register-class.js";
 /** @public */
 export {
