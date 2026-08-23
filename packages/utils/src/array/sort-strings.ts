8e4e228bfaa2fc4ec7729bb864c30cabbbf4ae74
Eugenio Depalo <eugeniodepalo@gmail.com>
2026-08-23 13:59:54 +0200

    Document sortStrings

--- packages/utils/src/array/sort-strings.ts
+++ packages/utils/src/array/sort-strings.ts
@@ -1,3 +1,4 @@
+/** Sorts strings by locale, leaving the given iterable untouched. */
 function sortStrings(values: Iterable<string>): string[] {
     return [...values].toSorted((a, b) => a.localeCompare(b));
 }
